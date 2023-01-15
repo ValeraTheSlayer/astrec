cbd585c13baa90d9ba534d61e626ca2b5eeccddc
Alexandr Chumakov <a.chumakov@flyingbird.kz>
2023-01-15 20:48:32 +0600

    cosmetics for code styling

--- project_tracking/project_tracking/views.py
+++ project_tracking/project_tracking/views.py
@@ -3,5 +3,9 @@ import datetime
 # from django.views.generic import View
 from django.http import JsonResponse
 
+
 def main_page(request):
-   return JsonResponse({'status': 'OK', 'message': 'test response', 'datetime': datetime.datetime.now().strftime("%H:%M:%S %d.%m.%Y")})
+    return JsonResponse(
+        {'status': 'OK',
+         'message': 'test response',
+         'datetime': datetime.datetime.now().strftime("%H:%M:%S %d.%m.%Y")})
