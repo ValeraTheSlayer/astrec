cd77e1cd02254778ce67847ec2f136320d7c6024
Alexandr Chumakov <a.chumakov@flyingbird.kz>
2023-01-18 00:02:37 +0600

    added models for Card and Approval

--- arec/approval/models.py
+++ arec/approval/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Approval(models.Model):
+    approving_person = models.ForeignKey(User, on_delete=models.CASCADE)
+    approved_at = models.DateTimeField(auto_now=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE)  # для отслеживания цепочки согласований
+
--- arec/card/models.py
+++ arec/card/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Card(models.Model):
+    """
+    Основная таблица заявки абонента
+    """
+    # ФИО поля
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    patronymic_name = models.CharField(max_length=50)
+
+    phone_number = models.PositiveIntegerField()
+
+    district = models.CharField(max_length=125)
+    street = models.CharField(max_length=125)
+    bldg = models.CharField(max_length=8)  # номер дома, CharField, т.к. включены дроби, буквы и т.п.
+    block = models.CharField(max_length=8)
+    apt_num = models.CharField(max_length=6)  # номер квартиры, CharField, т.к. тоже бывает с буквами
+
+    floor = models.PositiveIntegerField()
+    entrance = models.PositiveSmallIntegerField(default=1)  # номер подъезда
+    square = models.DecimalField(decimal_places=2, max_digits=6)  # площадь
+
+    connection_point = models.CharField(max_length=125)
+    power = models.CharField(max_length=8)
+    nominal = models.PositiveSmallIntegerField()
+    main_point = models.CharField(max_length=125)
+
+    counter_number = models.CharField(max_length=25)
+    counter_model = models.CharField(max_length=50)
+
+    contract = models.CharField(max_length=50)  # номер и дата договора
+    organization = models.CharField(max_length=250)
+    operator = models.CharField(max_length=250)  # ФИО оператора TODO: as relation with User model
+    comment = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # TODO: files
+    # TODO: relations to the Approval model
