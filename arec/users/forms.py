a02f2f3d293df6158f418555bcd0f7ebd91358f4
Valerij Ezhov <ezhov.fzzz@gmail.com>
2023-01-23 21:26:32 +0600

    add position field in registration form

--- arec/users/forms.py
+++ arec/users/forms.py
@@ -5,7 +5,7 @@ from .models import User
 class CreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'username', 'email')
+        fields = ('first_name', 'last_name', 'username', 'email', 'position',)
 
 
 class UserAuthenticationForm(AuthenticationForm):
