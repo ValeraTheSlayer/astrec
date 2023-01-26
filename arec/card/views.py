3bcd091aa02a48f441f53efaf572ef75849c1ce3
Valerij Ezhov <ezhov.fzzz@gmail.com>
2023-01-27 02:24:27 +0600

    code reformat

--- arec/card/views.py
+++ arec/card/views.py
@@ -20,11 +20,13 @@ def main_page(request):
     # TODO: пока отображает список заявок, в будущем д.б. дашборд
     return render(request, 'html/index.html')
 
+
 @my_view
 def card_list(request):
     cards = Card.objects.all()
     return render(request, 'card/card_list.html',
                   {'cards': cards})
+
 
 @my_view
 def card_detail(request, cid):
@@ -32,6 +34,7 @@ def card_detail(request, cid):
     scans = DocScan.objects.all().filter(card_id=cid)
     context = {'title': 'Детали карточки', 'card': card, 'scans': scans}
     return render(request, 'card/card_detail.html', context=context)
+
 
 @my_view
 def card_create(request):
@@ -44,18 +47,22 @@ def card_create(request):
                 logger.debug(request.FILES)
                 attachment = request.FILES.get(doc_type, None)
                 if attachment is not None:
-                    logger.debug(f'attached file {doc_type} detected, path {attachment}')
+                    logger.debug(
+                        f'attached file {doc_type} detected, path {attachment}')
                     scan = DocScan(doc_file=attachment,
                                    doctype=doc_type,
                                    card=card_obj)
                     scan.save()
-            messages.add_message(request, messages.SUCCESS, 'Заявка успешно создана!')
+            messages.add_message(request, messages.SUCCESS,
+                                 'Заявка успешно создана!')
             return HttpResponseRedirect('/cards/')
         else:
             logger.debug(f'invalid form {form.errors}')
-            messages.add_message(request, messages.ERROR, 'При добавлении карточки обнаружены ошибки! '
-                                                          'Проверьте заполнение.')
+            messages.add_message(request, messages.ERROR,
+                                 'При добавлении карточки обнаружены ошибки! '
+                                 'Проверьте заполнение.')
     else:
         form = CardForm()
     doc_types = [{'name': v[0], 'label': v[1]} for v in DOC_TYPES]
-    return render(request, 'card/card_create_form.html', {'form': form, 'doc_types': doc_types})
+    return render(request, 'card/card_create_form.html',
+                  {'form': form, 'doc_types': doc_types})
