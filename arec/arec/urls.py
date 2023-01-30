127d7c4d0007facefb3e82e158579f704189427b
Valerij Ezhov <ezhov.fzzz@gmail.com>
2023-01-30 16:53:18 +0600

    unite view functions and delete the old ones in urls file

--- arec/arec/urls.py
+++ arec/arec/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from card.views import main_page, card_create_individual, card_list_individuals, card_create_legal_entity, card_detail_individual, card_detail_legal_entity, card_list_legal_entities
+from card.views import main_page, card_create, card_list, card_detail
 from users.apps import UsersConfig
 
 
@@ -25,10 +25,10 @@ urlpatterns = [
     path('', main_page, name='index'),
     path('admin/', admin.site.urls),
     path('auth/', include('users.urls', namespace=UsersConfig.name)),
-    path('cards/individuals/', card_list_individuals, name='card_list_individuals'),
-    path('cards/legal-entities/', card_list_legal_entities, name='card_list_legal_entities'),
-    path('card/new/individual/', card_create_individual, name='new_card_individual'),
-    re_path(r'^card/individuals/(?P<cid>\d+)/$', card_detail_individual, name='card_individual'),
-    path('card/new/legal-entity/', card_create_legal_entity, name='new_card_legal_entity'),
-    re_path(r'^card/legal-entiti/(?P<cid>\d+)/$', card_detail_legal_entity, name='card_legal_entity'),
+    path('cards/individuals/', card_list, name='card_list_individuals'),
+    path('cards/legal-entities/', card_list, name='card_list_legal_entities'),
+    path('card/new/individual/', card_create, name='new_card_individual'),
+    re_path(r'^card/individuals/(?P<cid>\d+)/$', card_detail, name='card_individual'),
+    path('card/new/legal-entity/', card_create, name='new_card_legal_entity'),
+    re_path(r'^card/legal-entiti/(?P<cid>\d+)/$', card_detail, name='card_legal_entity'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- arec/card/views.py
+++ arec/card/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.contrib import messages
 from django.views.generic import View
 
-from .models import Card, DocScan, DOC_TYPES, CardIndividual
+from .models import Card, DOC_TYPES, CardIndividual
 from .forms import SubscriberCardForm as CardForm, CardIndividualForm, \
     CardLegalEntity, CardLegalEntityForm
 from .utils import my_view
@@ -24,109 +24,77 @@ def main_page(request):
 
 
 @my_view
-def card_list_individuals(request):
-    cards = CardIndividual.objects.select_related('card').all()
-    return render(request, 'card/card_list_individuals.html',
-                  {'cards': cards})
+def card_list(request):
+    link = request.get_full_path()
+    is_individual = 'individual' in link
+    if is_individual:
+        cards = CardIndividual.objects.select_related('card').all()
+    else:
+        cards = CardLegalEntity.objects.select_related('card').all()
+    return render(request, 'card/card_list.html',
+                  {'cards': cards, 'is_individual': is_individual})
+
 
 @my_view
-def card_detail_individual(request, cid):
-    card = CardIndividual.objects.select_related('card').get(pk=cid)
-    scans = DocScan.objects.all().filter(card_id=cid)
-    context = {'title': 'Детали карточки', 'card': card, 'scans': scans}
-    return render(request, 'card/card_detail_individuals.html', context=context)
+def card_detail(request, cid):
+    link = request.get_full_path()
+    is_individual = 'individual' in link
+    if is_individual:
+        card = CardIndividual.objects.select_related('card').get(pk=cid)
+    else:
+        card = CardLegalEntity.objects.select_related('card').get(pk=cid)
+    context = {'title': 'Детали карточки', 'card': card}
+    return render(request, 'card/card_detail.html', context=context)
+
 
 @my_view
-def card_list_legal_entities(request):
-    cards = CardLegalEntity.objects.select_related('card').all()
-    return render(request, 'card/card_list_legal_entities.html',
-                  {'cards': cards})
-
-@my_view
-def card_detail_legal_entity(request, cid):
-    card = CardLegalEntity.objects.select_related('card').get(pk=cid)
-    scans = DocScan.objects.all().filter(card_id=cid)
-    context = {'title': 'Детали карточки', 'card': card, 'scans': scans}
-    return render(request, 'card/card_detail_legal_entities.html', context=context)
-
-@my_view
-def card_create_individual(request):
+def card_create(request):
     if request.method == "POST":
-        form = CardForm(request.POST)
-        form_individual = CardIndividualForm(request.POST)
-        print('created date', form)
-        if form.is_valid() and form_individual.is_valid():
-            card_individual_obj = form_individual.save()
+        form = CardForm(request.POST or None)
+        link = request.get_full_path()
+        is_individual = 'individual' in link
+        form_secondary = CardIndividualForm(
+            request.POST or None) if is_individual else CardLegalEntityForm(
+            request.POST or None)
+
+        if form.is_valid() and form_secondary.is_valid():
+            card_secondary_obj = form_secondary.save()
             card_obj = form.save(commit=False)
-            card_obj.created_at = datetime.strptime(request.POST.get('created_date')
-                                                    + ', '
-                                                    + request.POST.get('created_time'),
-                                                    '%Y-%m-%d, %H:%M')
-            card_obj.individual_entity = card_individual_obj
+            if is_individual:
+                card_obj.created_at = datetime.strptime(
+                    request.POST.get('created_date') + ', ' + request.POST.get(
+                        'created_time'), '%Y-%m-%d, %H:%M')
+                card_obj.individual_entity = card_secondary_obj
+            else:
+                card_obj.legal_entity = card_secondary_obj
             card_obj.save()
-            for doc in DOC_TYPES:
-                doc_type = doc[0]
-                logger.debug(request.FILES)
-                attachment = request.FILES.get(doc_type, None)
-                if attachment is not None:
-                    logger.debug(
-                        f'attached file {doc_type} detected, path {attachment}')
-                    scan = DocScan(doc_file=attachment,
-                                   doctype=doc_type,
-                                   card=card_obj)
-                    scan.save()
+            attachment = request.FILES.get('file')
+            if attachment and attachment.name.endswith('.pdf'):
+                card_obj.file = attachment
+                card_obj.save()
+            elif attachment:
+                messages.add_message(request, messages.ERROR,
+                                     'Загружать можно только файлы формата .PDF')
+                return render(request, 'card/card_create_form.html',
+                              {'form': form,
+                               f'form_{"individual" if is_individual else "legal_entity"}': form_secondary,
+                               'is_individual': is_individual})
             messages.add_message(request, messages.SUCCESS,
                                  'Заявка успешно создана!')
-            return HttpResponseRedirect('/cards/individuals')
+            return HttpResponseRedirect(
+                f'/cards/{"individuals" if is_individual else "legal-entities"}')
         else:
-            logger.debug(f'invalid form {form.errors}')
             messages.add_message(request, messages.ERROR,
-                                 'При добавлении карточки обнаружены ошибки! '
-                                 'Проверьте заполнение.')
+                                 'При добавлении карточки обнаружены ошибки! Проверьте заполнение.')
     else:
         form = CardForm()
-        form_individual = CardIndividualForm()
-    doc_types = [{'name': v[0], 'label': v[1]} for v in DOC_TYPES]
-    return render(request, 'card/card_individual_create_form.html',
-                  {'form': form, 'form_individual': form_individual,
-                   'doc_types': doc_types})
+        link = request.get_full_path()
+        is_individual = 'individual' in link
+        form_secondary = CardIndividualForm(
+            request.POST or None) if is_individual else CardLegalEntityForm(
+            request.POST or None)
 
-@my_view
-def card_create_legal_entity(request):
-    if request.method == "POST":
-        form = CardForm(request.POST)
-        form_legal_entity = CardLegalEntityForm(request.POST)
-        if form.is_valid() and form_legal_entity.is_valid():
-            card_legal_entity_obj = form_legal_entity.save()
-            card_obj = form.save(commit=False)
-            card_obj.legal_entity = card_legal_entity_obj
-            card_obj.save()
-            for doc in DOC_TYPES:
-                doc_type = doc[0]
-                logger.debug(request.FILES)
-                attachment = request.FILES.get(doc_type, None)
-                if attachment is not None:
-                    logger.debug(
-                        f'attached file {doc_type} detected, path {attachment}')
-                    scan = DocScan(doc_file=attachment,
-                                   doctype=doc_type,
-                                   card=card_obj)
-                    scan.save()
-            messages.add_message(request, messages.SUCCESS,
-                                 'Заявка успешно создана!')
-            return HttpResponseRedirect('/cards/legal-entities')
-        else:
-            logger.debug(f'invalid form {form.errors}')
-            messages.add_message(request, messages.ERROR,
-                                 'При добавлении карточки обнаружены ошибки! '
-                                 'Проверьте заполнение.')
-    else:
-        form = CardForm()
-        form_legal_entity = CardLegalEntityForm()
-    doc_types = [{'name': v[0], 'label': v[1]} for v in DOC_TYPES]
-    return render(request, 'card/card_legal_entity_create_form.html',
-                  {'form': form, 'form_legal_entity': form_legal_entity,
-                   'doc_types': doc_types})
-
-
+        return render(request, 'card/card_create_form.html', {'form': form,
+                                                              f'form_{"individual" if is_individual else "legal_entity"}': form_secondary,
+                                                              'is_individual': is_individual})
 
