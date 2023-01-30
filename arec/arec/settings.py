bef24c1a4141cd6c9d0e9cb1d590320e0015d0e1
Valerij Ezhov <ezhov.fzzz@gmail.com>
2023-01-30 16:45:20 +0600

    new path for a media

--- arec/arec/settings.py
+++ arec/arec/settings.py
@@ -141,5 +141,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = '/code/files/docs/'
+MEDIA_ROOT = '/code/files/'
 MEDIA_URL = '/docs/'
