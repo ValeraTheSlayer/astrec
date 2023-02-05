58fbf35cce7ac02d4bfde2abfaa0ec70a6d1d892
Alexandr Chumakov <a.chumakov@flyingbird.kz>
2023-02-06 05:46:56 +0600

    added remark field in the Approval model

--- arec/approval/models.py
+++ arec/approval/models.py
@@ -6,5 +6,6 @@ from users.models import User
 class Approval(models.Model):
     approving_person = models.ForeignKey(User, on_delete=models.CASCADE)
     approved_at = models.DateTimeField(auto_now=True)
+    remark = models.TextField(blank=True)  # для каких-либо комментариев/пометок на этапе согласования
     parent = models.ForeignKey('self', on_delete=models.CASCADE)  # для отслеживания цепочки согласований
 
