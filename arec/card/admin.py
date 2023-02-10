4bc1e168c331c1bc6d1f034b4e4c55864de069f9
root <63d804f882eeee78a4cd58e4@bots.bitbucket.org>
2023-02-10 00:02:28 +0000

    fixes for the demo deploy

--- arec/card/admin.py
+++ arec/card/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from card.models import Card
+from approval.models import Approval
+from users.models import User
 
-# Register your models here.
+
+admin.site.register(User)
+admin.site.register(Card)
+admin.site.register(Approval)
+
