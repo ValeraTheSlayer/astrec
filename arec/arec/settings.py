bb58a3bcb05df49bd6417fb34839ad22aff69d1f
Valerij Ezhov <ezhov.fzzz@gmail.com>
2023-03-15 04:27:24 +0600

    add celery in django settings

--- arec/arec/settings.py
+++ arec/arec/settings.py
@@ -184,6 +184,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CELERY_BROKER_URL = 'redis://localhost:16379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:16379/0'
+
 MEDIA_ROOT = '/code/files/'
 MEDIA_URL = '/docs/'
 
